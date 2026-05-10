dd1d0abb7ace84a7d7c2e6805828cb0eafe43602
Marcin Mika <marcin.mika123@gmail.com>
2026-05-10 12:33:44 +0200

    Add User entity, repository and database migrations.

---
+++ backend/TripDrop/TripDrop.Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TripDrop.Domain.Entities;
+
+namespace TripDrop.Domain.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetByEmailAsync(string emial, CancellationToken cancellationToken);
+        Task AddAsync(User user, CancellationToken cancellationToken);
+        Task SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
---
+++ backend/TripDrop/TripDrop.Infrastructure/Migrations/20260428210046_AddUserAndTripsRelation.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TripDrop.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserAndTripsRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Username = table.Column<string>(type: "text", nullable: false),
+                    PasswordHash = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Trips_OwnerId",
+                table: "Trips",
+                column: "OwnerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Trips_Users_OwnerId",
+                table: "Trips",
+                column: "OwnerId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Trips_Users_OwnerId",
+                table: "Trips");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Trips_OwnerId",
+                table: "Trips");
+        }
+    }
+}
